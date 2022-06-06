b3576a1bc72b02a868ada6f1d83149c95685429c
Dell <Dell@DESKTOP-97HN4GD>
2022-06-06 00:13:34 -0600

    Agregando arquitectura basica

--- PANADEL/PANADEL.web/Controllers/ProductosController.cs
+++ PANADEL/PANADEL.web/Controllers/ProductosController.cs
@@ -1,4 +1,4 @@
-﻿using PANADEL.web.Models;
+﻿using PANADEL.BL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,23 +12,8 @@ namespace PANADEL.web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Pan Molde";
-
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Dulces de Leche";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Pan Blanco";
-
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
 
             return View(listadeProductos);
         }
